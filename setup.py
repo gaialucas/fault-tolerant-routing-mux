710320032c107d7f10c42d2246b9e64416ea8572
Gaia de Castro, Lucas <lucas.gaia.castro@usp.br>
2022-07-06 01:17:50 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+""""Setup package for installation"""
+from setuptools import setup
+
+setup(
+    name="fault_tolerant_routing_mux",
+    author="Gaia de Castro, Lucas",
+    author_email="lucas.gaia.castro@usp.br",
+    description="Analysis and simulation of fault tolerant routing multiplexers",
+    version="0.1.0",
+    packages="",
+    install_requires=['numpy'],
+    python_requires='>=3.8'
+)
